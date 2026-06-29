4d642abb57f95df41145b0972e00258c3499bd63
Roopa Ratheesh <rooparatheesh2000@gmail.com>
2026-06-29 16:54:08 +0530

    update

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -23,6 +23,23 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+subprojects {
+    project.pluginManager.withPlugin("com.android.library") {
+        extensions.configure<com.android.build.gradle.LibraryExtension>("android") {
+            if (namespace == null || namespace!!.isEmpty()) {
+                val manifest = File(projectDir, "src/main/AndroidManifest.xml")
+                if (manifest.exists()) {
+                    val content = manifest.readText()
+                    val packageMatch = Regex("package=\"([^\"]+)\"").find(content)
+                    if (packageMatch != null) {
+                        namespace = packageMatch.groupValues[1]
+                    }
+                }
+            }
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }

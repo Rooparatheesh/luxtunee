423d79d0ff28b233e0a1043367c146f6093fefb9
Roopa Ratheesh <rooparatheesh2000@gmail.com>
2026-07-15 15:38:46 +0530

    updates

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.luxtunee"
-        minSdk = flutter.minSdkVersion
+        minSdk = 21
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
